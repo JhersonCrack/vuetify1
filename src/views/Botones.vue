3da0426274c05aed97b89c6d96b2c9dfe0611796
JhersonCrack <metalero.jherson.zl@gmail.com>
2020-01-24 12:03:00 -0500

    2 componentes Home: maquetación    ,Botones: diseño de botones

---
+++ src/views/Botones.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-flex>
+                <v-btn>Boton 1</v-btn>
+                <v-btn color="primary">Boton 2</v-btn>
+                <v-btn dark color="blue">Boton 3</v-btn>
+                <v-btn color="secondary" flat>Boton 4</v-btn>
+                <v-btn color="success" large>Boton 5</v-btn>
+                <v-btn color="warning" small>Boton 5</v-btn>
+                <v-btn color="primary" large disabled>Boton 5</v-btn>
+                <v-btn color="info" depressed>Boton 6</v-btn>
+
+                <v-btn>
+                    <v-icon>favorite</v-icon>
+                </v-btn>
+                
+                <v-btn flat icon>
+                    <v-icon color="orange">add_circle</v-icon>
+                </v-btn>
+
+                <v-btn fab color="white">
+                    <v-icon color="black">add_circle</v-icon>
+                </v-btn>
+
+                <v-btn fab color="white" small>
+                    <v-icon color="black" large>add_circle</v-icon>
+                </v-btn>
+
+                <v-btn fab color="white" large>
+                    <v-icon color="black">add_circle</v-icon>
+                </v-btn>
+
+                <v-btn>
+                    <v-icon>search</v-icon>
+                    Buscar
+                </v-btn>
+
+                <v-btn color="success" outlined>
+                    <v-icon left>forum</v-icon>
+                    Boton X
+                </v-btn>
+
+                <v-btn color="success" outlined block rounded>
+                    <v-icon left>forum</v-icon>
+                    Boton X
+                </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        };
+    },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,142 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-container fluid> <!-- Contenido, como bootstrap -->
+        <v-layout> <!-- Que entre todo en una sola fila lo siguiente -->
+            <v-flex primary start space-around xs12> <!-- Si solo usamos v-flex, estamos indicando que se usara las 12 grillas -->
+                <v-layout start align-content-space-around>
+                    <h1>Logo de Youtube</h1>
+                    <h1>Input de busqeuda</h1>
+                    <h1>Log & Logout</h1>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+
+        <v-layout success>
+            <v-flex info xs2 style="height: 700px pl-1">
+                <h1 class=title>Página principal</h1>
+                <h1 class=title>Tendencias</h1>
+                <h1 class=title>Supscripciones</h1>
+                <h1>_________________</h1>
+            </v-flex>
+            <v-flex xs10>
+                <v-layout pa-5 wrap>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                    <v-flex xs2>
+                        <img src="juliocsm.png" alt="">
+                        <h1 class="subtitle-2">Julio Guzmán | La llama del amor | José Balta</h1>
+                        <p class="body-2 uppercase-text">José Balta</p>  
+                        <h2 class="subtitle-2">2,3 M visualizaciones .</h2> 
+                    </v-flex>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+
+    </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
